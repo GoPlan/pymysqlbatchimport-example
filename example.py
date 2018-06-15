481d8609005d8747a027063d199528964e207658
Duc-Anh Le <ducanh.ki2519@gmail.com>
2018-06-15 19:17:39 +0700

    Added fake data

--- example.py
+++ example.py
@@ -1,15 +1,15 @@
 import os
 
-def clear_finished():
+def repeat():
     folder = 'finished'
     for the_file in os.listdir(folder):
         file_path = os.path.join(folder, the_file)
         if os.path.isfile(file_path):
-            print(file_path)
             os.unlink(file_path)
 
 if __name__ == "__main__":
     import sys
     from pymysqlbatchimport.run import App
+
+    repeat()
     App.run()
-    clear_finished()
